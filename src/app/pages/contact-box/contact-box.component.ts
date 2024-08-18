79b95c178778d0d916e0bb062f74529a1464077c
Entangle Software <entanglesoftware@gmail.com>
2024-08-18 18:37:16 +0530

    minor fixes

--- src/app/pages/contact-box/contact-box.component.ts
+++ src/app/pages/contact-box/contact-box.component.ts
@@ -1,42 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { catchError, merge } from "rxjs";
-import { MatInputModule } from "@angular/material/input";
-import { MatButtonModule } from "@angular/material/button";
-import { trigger, state, style, transition, animate } from '@angular/animations';
-import { MatDialogClose } from "@angular/material/dialog";
-import { MatIconModule } from "@angular/material/icon";
-import { environment } from "../../../environments/environment";
-import Swal from "sweetalert2";
-import { HttpClient } from "@angular/common/http";
-import emailjs, { type EmailJSResponseStatus } from "@emailjs/browser";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+import { MatDialogClose } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { environment } from '../../../environments/environment';
+import Swal from 'sweetalert2';
+import { HttpClient } from '@angular/common/http';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, ReactiveFormsModule, MatInputModule, MatButtonModule, MatDialogClose, MatIconModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDialogClose,
+    MatIconModule,
+  ],
   templateUrl: './contact-box.component.html',
   styleUrl: './contact-box.component.scss',
   animations: [
     trigger('dialogAnimation', [
       state('void', style({ opacity: 0, transform: 'scale(0.7)' })),
       state('*', style({ opacity: 1, transform: 'scale(1)' })),
-      transition('void => *', [
-        animate('300ms ease-out')
-      ]),
-      transition('* => void', [
-        animate('200ms ease-in')
-      ]),
+      transition('void => *', [animate('300ms ease-out')]),
+      transition('* => void', [animate('200ms ease-in')]),
     ]),
   ],
-
 })
 export class ContactBoxComponent {
-
   email = new FormControl('', [Validators.required, Validators.email]);
-  firstName = new FormControl('', [Validators.required, Validators.minLength(2)]);
+  firstName = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2),
+  ]);
   connectButton: boolean = false;
   mailError = '';
   nameError = '';
@@ -47,7 +57,6 @@ export class ContactBoxComponent {
     merge(this.firstName.statusChanges, this.firstName.valueChanges)
       .pipe(takeUntilDestroyed())
       .subscribe(() => this.updateNameError());
-
   }
 
   updateNameError() {
@@ -70,11 +79,10 @@ export class ContactBoxComponent {
   }
 
   isFormValid(): boolean {
-    return this.firstName.valid && this.email.valid
+    return this.firstName.valid && this.email.valid;
   }
 
   sendEmail(e: Event) {
-
     e.preventDefault();
     const $options = {
       publicKey: environment.emailJsPublicKey,
@@ -89,9 +97,10 @@ export class ContactBoxComponent {
       lastName: '',
       email: this.email.value,
       message: 'Contacted through contact box popup',
-      contactNo: '9999999999'
-    }
-    const $serviceKey = 'service_6sqs94j';
+      contactNo: '9999999999',
+    };
+
+    const $serviceKey = 'service_ai7j4rw';
     const $templateKey = 'template_ovw9s7e';
 
     emailjs.send($serviceKey, $templateKey, form, $options).then(
@@ -103,7 +112,7 @@ export class ContactBoxComponent {
           confirmButtonColor: '#1483f8',
         });
         this.email.reset('');
-        this.firstName.reset('')
+        this.firstName.reset('');
       },
       (error: any) => {
         console.log(error);
@@ -115,8 +124,8 @@ export class ContactBoxComponent {
           confirmButtonColor: '#bb3624',
         });
         this.email.reset('');
-        this.firstName.reset('')
-      },
+        this.firstName.reset('');
+      }
     );
   }
 }
