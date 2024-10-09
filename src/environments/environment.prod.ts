7a32817cc2cee69df042cb1678804ab45b34aae3
Nishajoshi13 <nishajoshi132001@gmail.com>
2024-10-09 18:43:44 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,14 @@ export const environment = {
   apiBaseUrl: 'https://entanglesoftware.com/api',
   emailJsPublicKey: 'y3kLYt5hqOwZvoUBg',
   emailServiceId: 'service_6sqs94j',
+  firebaseConfig : {
+    apiKey: "AIzaSyAIdoanfbMKUNyVye-Br6lMuaAKKMZ4iLY",
+    authDomain: "espl-website-3b854.firebaseapp.com",
+    databaseURL: "https://espl-website-3b854-default-rtdb.firebaseio.com",
+    projectId: "espl-website-3b854",
+    storageBucket: "espl-website-3b854.appspot.com",
+    messagingSenderId: "539993972584",
+    appId: "1:539993972584:web:9ac2170c8d9106528580dd",
+    measurementId: "G-HXTJCFDX8K"
+  }
 };
