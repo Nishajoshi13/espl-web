92beb95a3047223637c1972300f3a7fca58626c3
Nishajoshi13 <nishajoshi132001@gmail.com>
2024-10-24 12:50:14 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { environment } from '../environments/environment';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
-  declarations: [AppComponent,],
+  declarations: [AppComponent,
+],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,7 +21,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     MatDialogModule,
     AngularFireModule.initializeApp(environment.firebaseConfig), // Initialize Firebase
     AngularFireDatabaseModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: !isDevMode(),
+  enabled: environment.production,
   // Register the ServiceWorker as soon as the application is stable
   // or after 30 seconds (whichever comes first).
   registrationStrategy: 'registerWhenStable:30000'
